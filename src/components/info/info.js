49bcb07eceab0cc7f9fcdce5ff0982743eb58bc9
yurchenko08 <yurchenko08@gmail.com>
2022-10-19 20:44:42 +0300

    added attr and text

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export default function Info(){
-    return /* some code */;
+export default function Info() {
+  return <div data-testid='element-info'>JSX produces React 'elements'</div>;
 }
